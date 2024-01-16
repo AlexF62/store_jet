a80e8fc9ca00e1ff9144938b0fae9d4ac2570fa9
Aleksandr Fomichev <fomichevae28@gmail.com>
2024-01-16 21:02:30 +0100

    feature/create-tabs

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,15 +1,33 @@
-import Slider from '@/components/slider/Slider'
+import Slider from 'react-slick'
 import './globals.scss'
 import Banner from '@/components/banner/Banner'
+import TabsPage from '@/components/tabs/Tabs'
+
+
 
 export default function Page() {
-  return <section className="banner-section">
-    <div className="container">
-      <div className="banner-section__inner">
-            <Slider/>
-            <Banner/>
+  
+  return (
+       <><section className="banner-section page-section">
+      <div className="container">
+        <div className="banner-section__inner">
+          <Slider />
+          <Banner />
+         
+
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+    
+    <section className="search page-section">
+        <div className="container">
+        <TabsPage/>
+        </div>
+      </section></>
+
+  )
+  
+  
+ 
 }
  
